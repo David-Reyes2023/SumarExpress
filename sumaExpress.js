5beecabdb6ff39e920ec36233d09c854ba208559
David Reyes Moya <david.reyes@tecnologicoliceocristiano.edu.ec>
2024-04-30 18:02:46 -0500

    SumaExpress

---
+++ sumaExpress.js
@@ -0,0 +1,39 @@
+const servidor = servidor('http');
+
+const server = servidor.createServer((req,resp)=>{
+    if(req.method == 'POST'){
+        let body ='';
+        req.on('data',(cunk)=>{
+            body += chunk;
+        });
+
+        req.on('end', ()=>{
+            let arrayNum = body.split('&');
+            console.log(arrayNum);
+            let num1 = arrayNum[0].split('=')[1];
+            console.log(num1);
+            let num2 = arrayNum[1].split('=')[1];
+
+            let num = parseInt(num1);
+            let numdos = parseInt(num2);
+            let suma= num + numdos;
+
+            resp.end('R1 =' + suma);
+            return;
+        })
+    }
+    else{
+        resp.setHeader('Content-Type','text/html');
+        resp.end(`<form method="POST">
+                <input name ="num1" type="number"/>
+                <br/>
+                <br/>
+                <input name ="num2" type="number"/>
+                <br/>
+                <br/>
+                <button>Calcular<button/>
+                </form>`)
+    }
+});
+
+server.listen(5001);
